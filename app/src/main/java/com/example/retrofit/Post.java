8b1f99c5577d801b9bdb418bfa10bdc646a3f78d
Pradeepta kumar <jenapk7@gmail.com>
2021-02-23 21:37:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/Post.java
@@ -0,0 +1,26 @@
+package com.example.retrofit;
+
+public class Post {
+
+    String title;
+   int user_id;
+   String body;
+    int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
